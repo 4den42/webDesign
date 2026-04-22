874e21639a4ab69d7fbe6a0609b96f85129550b9
Aden Hartman <124917291+4den42@users.noreply.github.com>
2026-04-22 12:53:31 -0700

    Cleaned up

--- simpleForm/values.php
+++ simpleForm/values.php
@@ -1,7 +1,7 @@
 <?php
 $boxes = $_POST["checkboxes"];//receiving name field value in $name variable
 $radios = $_POST["radios"];
-echo "what up homie";
+echo "Roger";
 echo "<table>";
 foreach($boxes as $x => $val){
     echo "$x = $val<br>";
